65e743b19be80466e01f2bbe296002ba34823138
JJ Thomson <s24c1091zk@s.chibakoudai.jp>
2025-11-25 16:42:53 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hoge',
+    maintainer_email='qwertyuiop1103566@gmail.com',
+    description='a pakage for practice',
+    license='GPL3.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
+        ],
+    },
+)
